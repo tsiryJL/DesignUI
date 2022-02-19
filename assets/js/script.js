d8c4509642bb097455220e4fcd136d95b1d663a3
tsiryjl <tsiry.razanakoto@gmail.com>
2022-02-19 13:52:47 +0300

    resolve img not showing

--- assets/js/script.js
+++ assets/js/script.js
@@ -293,32 +293,3 @@ function easeInOut(t) {
   return (Math.sin(t / period + 100) + 1) /2;
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  var phrases = shuffleArray(["Connexion au serveur", "Requêtes HTTP ", "Chargement des scripts", "Résolution d'IP", "Check mémoire cache", "Web Service", "Chargement des données", "Autonomiser l'humanité", "Génerer la pages", "Simulation des flux", "Récupération des données", "Compile SCSS ✨", "Garder le sourir 😊", "Code 200 ✔️", "Bienvenue à toi", "Récupération des données"]);
-  addPhrasesToDocument(phrases);
-  var start_time = new Date().getTime();
-  var upward_moving_group = document.getElementById("phrases");
-  upward_moving_group.currentY = 0;
-  var checks = phrases.map(function(_, i) { 
-    return {check: document.getElementById(checkmarkIdPrefix + i), circle: document.getElementById(checkmarkCircleIdPrefix + i)};
-  });
-  function animateLoading() {
-    var now = new Date().getTime();
-    upward_moving_group.setAttribute("transform", "translate(0 " + upward_moving_group.currentY + ")");
-    upward_moving_group.currentY -= 1.35 * easeInOut(now);
-    checks.forEach(function(check, i) {
-      var color_change_boundary = - i * verticalSpacing + verticalSpacing + 15;
-      if (upward_moving_group.currentY < color_change_boundary) {
-        var alpha = Math.max(Math.min(1 - (upward_moving_group.currentY - color_change_boundary + 15)/30, 1), 0);
-        check.circle.setAttribute("fill", "rgba(255, 255, 255, " + alpha + ")");
-        var check_color = [Math.round(255 * (1-alpha) + 120 * alpha), Math.round(255 * (1-alpha) + 154 * alpha)];
-        check.check.setAttribute("fill", "rgba(255, " + check_color[0] + "," + check_color[1] + ", 1)");
-      }
-    })
-    if (now - start_time < 30000 && upward_moving_group.currentY > -710) {
-      requestAnimationFrame(animateLoading);
-    }
-  }
-  //animateLoading();
-});
-
